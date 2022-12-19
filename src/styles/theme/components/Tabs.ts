9a0d959c1fc451eaaa332664838393acd38e75f5
michyaraque <michyaraque@gmail.com>
2022-12-20 00:36:09 +0100

    feat: :sparkles: added illustration to the card creator and another types to the selector

---
+++ src/styles/theme/components/Tabs.ts
@@ -0,0 +1,30 @@
+import { ChakraTheme, DeepPartial, defineStyle, theme, Theme } from "@chakra-ui/react";
+
+const Tabs: DeepPartial<ChakraTheme["components"]["Tabs"]> = {
+    variants: {
+        board: (props) => ({
+            ...theme?.components?.Tabs?.variants?.enclosed(props),
+            tab: {
+                border: '2px solid',
+                borderColor: 'transparent',
+                // use colorScheme to change background color with dark and light mode options
+                backgroundColor: "rgba(62, 71, 81, 0.2)",
+                borderBottom: 'none',
+                borderTopRadius: '4px',
+                _selected: {
+                  bg: "rgba(98, 109, 122, 0.2)",
+                },
+              },
+            tablist: {
+                ml: 3,
+                borderBottom: '2x solid',
+                borderColor: 'inherit',
+              },
+              tabpanel: {
+                backgroundColor: "rgba(98, 109, 122, 0.2)",
+              },
+        }),
+    }
+};
+
+export default Tabs;
