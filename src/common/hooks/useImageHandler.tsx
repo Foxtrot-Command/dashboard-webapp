19c8f8f8082cef72f65e30bc77d0808ec3e24784
michyaraque <michyaraque@gmail.com>
2023-05-31 13:00:09 +0200

    feat: :sparkles: add a way to create a watermark images to post on artists RRSS

--- src/common/hooks/useImageHandler.tsx
+++ src/common/hooks/useImageHandler.tsx
@@ -2,26 +2,39 @@ import { useState } from "react";
 
 const useImageHandler = () => {
   const [selectedImage, setSelectedImage] = useState<string | any>(null);
+  const [imageFile, setImageFile] = useState<File | null>(null);
+  const [fileName, setFileName] = useState<string | null>(null);
 
-  const imageHandler = (event: Event | null) => {
+  const imageHandler = (event: React.ChangeEvent<HTMLInputElement> | null) => {
     if (event === null) {
       setSelectedImage(null);
       return;
     }
+    const file = event.target.files && event.target.files[0];
+    if (file) {
+      setImageFile(file);
 
-    const reader = new FileReader();
-    reader.onload = function (e: any) {
-      setSelectedImage(e.target.result);
-    };
+      const reader = new FileReader();
+      reader.onload = function (e: any) {
+        setSelectedImage(e.target.result);
+      };
 
-    if (event.target instanceof HTMLInputElement && event.target.files) {
-      if (!event.target.files[0]) return;
+      const fileNameWithoutExtension = file.name.split('.').slice(0, -1).join('.');
+      setFileName(fileNameWithoutExtension);
 
-      reader.readAsDataURL(event.target.files[0]);
+      if (event.target instanceof HTMLInputElement && event.target.files) {
+        if (!event.target.files[0]) return;
+
+        reader.readAsDataURL(event.target.files[0]);
+      }
+    } else {
+      setSelectedImage(null);
+      setImageFile(null); // Reset the watermarked image state when the image is deleted
+      setFileName(null);
     }
   };
 
-  return { selectedImage, imageHandler };
+  return { imageFile, fileName, selectedImage, imageHandler };
 };
 
 export default useImageHandler;
